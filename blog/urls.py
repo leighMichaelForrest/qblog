8bbc1ecbf5d22dbfa4577f28ac5b589484083633
Leigh Forrest <leighmforrest@comcast.net>
2018-02-03 20:05:34 -0500

    Tutorial completed

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+from . import views, feed
+
+
+urlpatterns = [
+    url(r'^feed/$', feed.LatestPosts(), name="feed"),
+    url(r'^$', views.BlogIndex.as_view(), name="index"),
+    url(r'^(?P<slug>\S+)$', views.BlogDetail.as_view(), name="entry_detail")
+]
